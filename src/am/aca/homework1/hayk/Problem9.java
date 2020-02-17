f8a3268e9590c2d0e2243d73ef7fc5bc438a2530
vardanaivazian <vardan.aivazian@eyesoftinc.com>
2020-02-17 12:25:52 +0400

    fix: Hayks Homework1 Problem9

--- src/am/aca/homework1/hayk/Problem9.java
+++ src/am/aca/homework1/hayk/Problem9.java
@@ -1,20 +1,14 @@
 package am.aca.homework1.hayk;
 import java.util.Scanner;
-
 public class Problem9 {
 
-    public static void main(String[] args) {
-	Scanner a = new Scanner (System.in);
-	Scanner b = new Scanner (System.in);
-	Scanner c = new Scanner (System.in);
-	int d = a.nextInt();
-	int e = b.nextInt();
-	int f = c.nextInt();
-	if ( d > e && d > f)
-	    System.out.println(" The average number is = " + d);
-	else if ( e > d && e > f )
-	    System.out.println(" The average number is = " + e);
-	else
-        System.out.println(" The average number is = " + f);
-    }
+	public static void main(String[] args) {
+		Scanner a = new Scanner (System.in);
+		int d = a.nextInt();
+		int e = a.nextInt();
+		int f = a.nextInt();
+		float b;
+		b = ( d + e + f)/3f;
+		System.out.println("The average number is " + b);
+	}
 }
